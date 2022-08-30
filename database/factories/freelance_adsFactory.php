34ae83a959c063737d2ae7df504946863ead7fca
Levy Crols <103933738+levyCro@users.noreply.github.com>
2022-08-30 12:13:24 +0200

    adding migrate files

---
+++ database/factories/freelance_adsFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class freelance_adsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+            'user_id' => User::factory(),
+            'category_id' => Category::factory(),
+            'slug' => $this->faker->slug(),
+            'title' => $this->faker->sentence(),
+            's' => $this->faker->slug(),
+            'body' => '<p>' . implode('</p><p>', $this->faker->paragraphs(6)) . '</p>',
+            
+            
+        ];
+    }
+}
