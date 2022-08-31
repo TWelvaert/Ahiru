faa9af60ca6de991fde0ab4f5f3f71cdbea7f139
soneoso <eli.dekeersgieter@gmail.com>
2022-08-31 10:29:28 +0200

    create landing

---
+++ resources/js/Pages/Landing.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+
+<div class="main-container bg-black min-h-screen">
+    <div class="index-container">
+        <div class="h1-landing">
+            <div class="h1-section">
+                <h1 class="">AHIRU</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+</template>
+
