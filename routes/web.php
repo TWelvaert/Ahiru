b02067fd5bac96af5e9f7e2c10a4b1f0be17e084
gychem <gychem.baert@hotmail.com>
2022-09-13 11:20:07 +0200

    Edit account settings route name

--- routes/web.php
+++ routes/web.php
@@ -51,6 +51,8 @@ Route::get('/advertisement/{freelanceAdvertisement:slug}', [FreelanceAdsControll
 
 Route::get('settings/account', [\App\Http\Controllers\Auth\RegisteredUserController::class, 'edit'])->name('settings/account');
 Route::post('settings/account', [\App\Http\Controllers\Auth\RegisteredUserController::class, 'update'])->name('settings/account');
+Route::get('settings/uploads', [\App\Http\Controllers\UploadsController::class, 'index'])->name('settings/uploads');
+
 
 Route::get('/about', function () {
     return Inertia::render('About');
