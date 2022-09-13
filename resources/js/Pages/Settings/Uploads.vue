b3d4b19bdcd4d5f0f9fd60c4b72d82c2b9c3a55c
gychem <gychem.baert@hotmail.com>
2022-09-13 11:47:58 +0200

    Add uploads (page, controller, route, model)

---
+++ resources/js/Pages/Settings/Uploads.vue
@@ -0,0 +1,27 @@
+<script setup>
+    import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
+
+    let data = defineProps({
+        uploads_images: Array,
+        uploads_audio: Array
+    });
+
+</script>
+    
+<template>
+    <div class="bg-white">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Your Uploads
+        </h2>
+
+        <b>Images</b>
+        <div v-for="image in data['uploads_images']">
+            {{ image }}
+        </div>
+
+        <b>Audio</b>
+        <div v-for="audio in data['uploads_audio']">
+            {{ audio }}
+        </div>
+    </div>
+</template>
