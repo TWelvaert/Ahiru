a771418df7dd35c4fcd2ab411767b84fdcc810c6
gychem <gychem.baert@hotmail.com>
2022-09-20 17:25:48 +0200

    Add admin users list page

---
+++ resources/js/Pages/Admin/Users/List.vue
@@ -0,0 +1,19 @@
+<script setup>
+    import AdminPanelLayout from "@/Layouts/AdminPanelLayout.vue";  
+    
+    let data = defineProps({
+        users: Array,
+    });
+
+    console.log(data["users"])
+
+</script>
+    
+<template>
+    <AdminPanelLayout>
+        User List
+        <table v-for="user in users.data">
+            <tr><td> {{ user.id  }} - </td><td> {{ user.name  }} - </td><td> {{ user.email  }} - </td><td> <a :href="`users/${user.slug}/edit`">EDIT</a> - </td><td> DELETE</td></tr>
+        </table>
+    </AdminPanelLayout>
+</template>
