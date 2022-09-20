b58c80348815f3b069a0d8a7525ba3cf72c411f8
gychem <gychem.baert@hotmail.com>
2022-09-20 11:30:33 +0200

    Add admin panel (controller,model,pages) WIP

---
+++ resources/js/Pages/Admin/Panel.vue
@@ -0,0 +1,24 @@
+<script setup>
+    import BreezeAuthenticatedLayout from "@/Layouts/Authenticated.vue";  
+    import AdminSideBar from "@/Components/AdminSideBar.vue";  
+    import { Head } from "@inertiajs/inertia-vue3";
+</script>
+    
+<template>
+    <Head title="Admin" />
+    <BreezeAuthenticatedLayout>
+
+        <div class="flex">
+            <AdminSideBar class="flex-col" />
+            <div>
+
+                Welcome to the Admin Panel
+
+            </div>
+        </div>
+        
+
+
+
+    </BreezeAuthenticatedLayout>
+</template>
