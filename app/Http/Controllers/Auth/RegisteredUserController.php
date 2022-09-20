edc88972dc39c2ca646a3ddf2888fc4961497993
gychem <gychem.baert@hotmail.com>
2022-09-20 19:37:56 +0200

    add user rank to store function

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -48,6 +48,7 @@ class RegisteredUserController extends Controller
             'slug' => str()->slug($request->name),
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'rank' => 'user',
         ]);
 
         event(new Registered($user));
