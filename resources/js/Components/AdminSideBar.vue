0466f02eb0a3a93c91b61cc88eabc1985c75ff6f
gychem <gychem.baert@hotmail.com>
2022-09-20 17:23:44 +0200

    Update nav links

--- resources/js/Components/AdminSideBar.vue
+++ resources/js/Components/AdminSideBar.vue
@@ -1,11 +1,19 @@
+<script setup>
+    import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
 <template>
     <div style="border-right: solid 1px black">
         <h2><b><u>ADMIN PANEL</u></b></h2><br>
-        <b>News Articles</b><br>
-        <a href="/admin/news/create">Create</a><br>
-        - Edit<br><br>
+        <Link :href="route('admin')">Dashboard</Link><br><br>
+        <Link :href="route('admin')">Collaborations</Link><br><br>
+        <b>News</b><br>
+        <Link :href="route('admin/news')">Articles</Link><br>
+        <Link :href="route('admin/news/create')">Create</Link><br><br>
         <b>Users</b><br>
-        - Edit<br><br>
+        <Link :href="route('admin/users')">List</Link>
+        <br><br>
         <b>Uploadsmanager</b><br>
+        <a href="/admin" class="text-orange-600">Uploads</a><br>
     </div>
 </template> 
