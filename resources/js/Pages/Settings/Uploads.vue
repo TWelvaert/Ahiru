2572ccf63a2a8328c25ae1508eb1249d12c74a6f
Gychem Baert <gychem.baert@hotmail.com>
2022-09-22 16:17:19 +0200

    update styling

--- resources/js/Pages/Settings/Uploads.vue
+++ resources/js/Pages/Settings/Uploads.vue
@@ -3,7 +3,7 @@
 
 <template>
     <Dashboard>
-    <div class="bg-white">
+    <div class="bg-white w-full h-full m-4 p-2">
         <form @submit.prevent="submit" enctype="multipart/form-data">
             <div>
                 <div class="flex justify-center items-center w-full">
