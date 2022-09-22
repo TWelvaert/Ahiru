ebc8f016376583975f1e40ac29979bf682c96ece
Gychem Baert <gychem.baert@hotmail.com>
2022-09-22 12:28:56 +0200

    improve structure, define user prop

--- resources/js/Pages/Likes.vue
+++ resources/js/Pages/Likes.vue
@@ -1,17 +1,12 @@
 <script setup>
     import Dashboard from '@/Pages/Dashboard.vue';
+    let data = defineProps({
+        user: Array,
+    });
+</script>
 
-
-    </script>
-
-
-
-
-    <template>
+<template>
     <Dashboard>
-
-
-
-
+        Likes Page
     </Dashboard>
-    </template>
+</template>
