268f3868bc58a7df9323ab82fdb77d21e94366ea
Thomas Welvaert <thomaswelvaert@hotmail.com>
2022-09-26 09:40:54 +0200

    remove active and member since button

--- resources/js/Pages/Profile.vue
+++ resources/js/Pages/Profile.vue
@@ -58,32 +58,22 @@
                                 Reprehenderit, eligendi dolorum sequi illum qui unde aspernatur non deserunt</p>
                             
                             
-                            <ul
-                                class="bg-gray-100 text-gray-600 hover:text-gray-700 hover:shadow py-2 px-3 mt-3 divide-y rounded shadow-sm">
-                                <li class="flex items-center py-3">
-                                    <span>Status</span>
-                                    <span class="ml-auto"><span
-                                            class="bg-green-500 py-1 px-2 rounded text-white text-sm">Active</span></span>
-                                </li>
-                                <li class="flex items-center py-3">
-                                    <span>Member since</span>
-                                    <span class="ml-auto">Nov 07, 2016</span>
-                                </li>
-                            </ul>
+                           
                         </div>
                         <!-- End of profile card -->
-                        <div class="my-4"></div>
-                        <!-- Following card -->
-                        <div class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                            <div class="flex items-center space-x-3 font-semibold text-gray-900 text-xl leading-8">
-                                <span class="text-green-500">
+                        <div class="my-4 flex items-center space-x-3 font-semibold text-gray-900 text-xl py-1"><span class="text-green-500">
                                     <svg class="h-5 fill-current" xmlns="http://www.w3.org/2000/svg" fill="none"
                                         viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                             d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                                     </svg>
                                 </span>
-                                <span>Following</span>
+                                <span>Following</span></div>
+                        <!-- Following card -->
+                        <div class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            
+                            <div class="flex items-center space-x-3 font-semibold text-gray-900 text-xl leading-8">
+                                
                             </div>
                             <div class="grid grid-cols-3">
                                 <div class="text-center my-2">
@@ -167,22 +157,23 @@
                         
                         <!-- End of Advertisements section -->
     
-                        <div class="my-4"></div>
+                        <div class="my-4 flex items-center space-x-3 font-semibold text-gray-900 text-xl py-1"></div>
     
                         <!-- Uploads and Something? -->
-                        <div class="bg-white block p-6 max-w-sm rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-full">
-    
-                            <div class="grid grid-cols-2">
-                                <div>
-                                    <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8 mb-3">
-                                        <span clas="text-green-500">
+                        <span clas="text-green-500">
                                             <svg class="h-5" xmlns="http://www.w3.org/2000/svg" fill="none"
                                                 viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                     d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                                             </svg>
                                         </span>
-                                        <span class="tracking-wide">Uploads</span>
+                                        <span class="tracking-wide font-semibold text-gray-900 text-xl">Uploads</span>
+                        <div class="bg-white block p-6 max-w-sm rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-full">
+    
+                            <div class="grid grid-cols-2">
+                                <div>
+                                    <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8 mb-3">
+                                        
                                     </div>
                                     <ul class="list-inside space-y-2">
                                         <li>
