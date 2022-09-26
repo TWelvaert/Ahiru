a80670e936808929c136e37730c47d87f1573fab
Gychem Baert <gychem.baert@hotmail.com>
2022-09-26 16:06:27 +0200

    improve styling

--- resources/js/Pages/Settings/Uploads.vue
+++ resources/js/Pages/Settings/Uploads.vue
@@ -1,33 +1,38 @@
 <template>
-        <div class="bg-white w-full h-full m-4 p-2">
+        <div class="bg-white w-full h-full p-2">
             <form id="form" @submit.prevent="submit" novalidate enctype="multipart/form-data">
                 <div>
                     <div class="flex justify-center items-center w-full">
                         <label for="dropzone-file" class="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                      
                             <div class="flex flex-col justify-center items-center pt-5 pb-6">
-                                <svg aria-hidden="true" class="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-                                <p class="text-xs text-gray-500 dark:text-gray-400">Images: SVG, PNG, JPG, GIF</p>
-                                <p class="text-xs text-gray-500 dark:text-gray-400">Audio: MP3, WAV</p>
-                                <div id="filesPreview" style="display: none;" class="bg-slate-600 rounded text-white absolute gap-2 m-4 p-2"></div>
+                                <div id="upload_info">
+                                    <svg aria-hidden="true" class="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                                    <p class="text-xs text-gray-500 dark:text-gray-400">Images: SVG, PNG, JPG, GIF</p>
+                                    <p class="text-xs text-gray-500 dark:text-gray-400">Audio: MP3, WAV</p>
+                                </div>
+                                <div id="filesPreview" style="display: none;" class="bg-white roundedabsolute gap-2 m-4 p-2"></div>
                             </div>
+                            
                             <BreezeInput id="dropzone-file" @input="form.uploads = $event.target.files;loadFilePreview($event.target.files)" type="file" class="hidden" multiple />
                         </label>
                     </div>
-                    <div class="float-right mt-2 mr-2">
+                    <div class="float-right mt-2">
                         <BreezeButton name="form" class="ml-4" :class="{ 'opacity-25': this.$refs.processing }" :disabled="this.$refs.processing">
                             Upload
                         </BreezeButton>
                     </div>
                 </div>
             </form>
-            <BreezeButton  @click="resetSelection()" name="form" class="ml-4" :class="{ 'opacity-25': this.$refs.processing }" :disabled="this.$refs.processing">
-                            RESET SELECTION
-                        </BreezeButton>
-            <BreezeLabel for="filter" value="Filter" />
-            <BreezeInput id="filter" placeholder="Search file name" type="text" />
-      
-            
+
+            <div class="flex mt-2">
+                <BreezeInput id="filter" placeholder="Search file name" type="text" />
+                <BreezeButton  @click="resetSelection()" name="form" class="ml-4" :class="{ 'opacity-25': this.$refs.processing }" :disabled="this.$refs.processing">
+                    RESET SELECTION
+                </BreezeButton>
+            </div>
+
             <div id="all_uploads" class="gap-4 mt-4">
                 <div class="flex gap-4">
                     <div v-for="upload in this.user_uploads">
@@ -36,7 +41,7 @@
                                 <header class="bg-gray-100 text-black rounded-t p-1 w-40 truncate">
                                     <Link class="cursor-pointer" v-bind:href="`/settings/uploads/delete/${upload.id}`">Delete</Link>  
                                 </header>
-                                <main>
+                                <main class="relative">
                                     <img class="w-40 h-40 z-0 cursor-pointer" :src="`/${upload.path}/${upload.name}`" />
                                 </main>
                                 <footer class="flex justify-between bg-gray-100 rounded-b p-1 w-40 truncate">
@@ -48,13 +53,13 @@
                 </div>
                 <div v-for="upload in this.user_uploads" class="mt-4">
                     <div v-if="upload.type == 'audio'" class="w-40" @click="e => select(e)" :id="upload.id" :class="{active: isActive}">
-                        <header class="bg-gray-100 text-black rounded-t p-1 w-40 truncate">
+                        <header class="bg-gray-100 text-black rounded-t p-1 w-40 truncate text-center">
                             <Link class="cursor-pointer" v-bind:href="`/settings/uploads/delete/${upload.id}`">Delete</Link>  
                         </header>
                         <main>
                             <img  class="cursor-pointer w-40 h-40" src="/assets/img/play_audio.png" v-on:click="$callMusicPlayer(upload.name)"/>
                         </main>
-                        <footer class="flex justify-between bg-gray-100 rounded-b p-1 w-40 truncate">
+                        <footer class="bg-gray-100 rounded-b p-1 w-40 truncate">
                             {{ upload.original_name }}
                         </footer>
                     </div>
@@ -106,13 +111,12 @@ function select(e) {
 
     if(e.path[2].getAttribute('id') > 0) {
         e.path[2].classList.add('active_file');
-
-
         if(document.querySelector(`#nr_${e.path[2].getAttribute('id')}`)) {
 
         } else {
             selected_file_nr += 1;
-            e.path[2].innerHTML += `<div id="nr_${e.path[2].getAttribute('id')}" class="absolute selectedFile">${selected_file_nr}</div>`;
+            // console.log(e.path[1])
+            e.path[1].innerHTML += `<div id="nr_${e.path[2].getAttribute('id')}" class="top-0 content-center absolute selectedFile min-w-full min-h-full flex justify-center items-center text-white text-3xl">${selected_file_nr}</div>`;
             file_id = e.path[2].id;
         }
 
@@ -139,11 +143,13 @@ function loadFilePreview(files) {
         if(files.length > 1) {
             for(let i=0; i<files.length; i++) {
                 let source = URL.createObjectURL(files[i]);
+                document.querySelector('#upload_info').style.display = 'none';
                 document.querySelector('#filesPreview').style.display = 'flex';
                 document.querySelector('#filesPreview').innerHTML += `<img class="w-40 h-40" src="${source}"/>`;
             };
         } else {
             let source = URL.createObjectURL(files[0]);
+            document.querySelector('#upload_info').style.display = 'none';
             document.querySelector('#filesPreview').style.display = 'flex';
             document.querySelector('#filesPreview').innerHTML += `<img class="w-40 h-40" src="${source}"/>`;
         }
