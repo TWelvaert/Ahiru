72d4ab38d6d3b7290bc8322f06eb8b683578475d
gychem <gychem.baert@hotmail.com>
2022-09-27 17:06:20 +0200

    fix errors after merge

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -18,7 +18,7 @@
                         </BreezeNavLink>
                     </div>
 
-                    <Searchbar/>
+                    <!-- <Searchbar/> -->
 
                     <div class="hidden  space-x-8 sm:-my-px sm:ml-10 sm:flex">
                         <BreezeNavLink :href="route('advertisements')"
@@ -109,103 +109,8 @@
 
                 </div>
 
-                <!-- Navigation Links -->
-                <BreezeNavLink :href="route('home')" :class="{ 'active': $page.url === '/home' }">
-                    Home
-                </BreezeNavLink>
-            
+              
 
-            <div class="pt-3">
-                <input
-                    class="border-white opacity-50 focus:border-white focus:ring-0 rounded-full bg-black text-l text-white h-10 pr-32 px-8"
-                    type="text" id="search" placeholder="SEARCH FOR ARTISTS" />
-            </div>
-
-            <div class="hidden  space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                <BreezeNavLink :href="route('advertisements')" :class="{ 'active': $page.url === '/advertisements' }">
-                    Advertisements
-                </BreezeNavLink>
-
-                <BreezeNavLink class="" :href="route('music')" :class="{ 'active': $page.url === '/music' }">
-                    Music
-                </BreezeNavLink>
-            </div>
-
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
-
-                <!-- Settings dropdown username -->
-
-                <div class="ml-3 relative">
-                    <BreezeDropdown align="right" width="48">
-                        <template #trigger>
-
-                            <span class="inline-flex rounded-md">
-
-                                <button type="button"
-                                    class="font-momentum inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-full text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                                    <img class="h-7 w-7 m-1 rounded-full" src="../../assets/images/waterloop.gif"
-                                        alt="">
-                                    {{ user.name}}
-                                    <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 20 20" fill="currentColor">
-                                        <path fill-rule="evenodd"
-                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-                                </button>
-
-                            </span>
-                        </template>
-
-                        <!-- Settings dropdown -->
-
-                        <template #content>
-
-                            <BreezeDropdownLink :href="`/profile/${user.slug}`"
-                                :class="{ 'active': $page.url === '/profile' }">
-                                Profile
-                            </BreezeDropdownLink>
-                            <BreezeDropdownLink :href="route('likes')" :class="{ 'active': $page.url === '/likes' }">
-                                Likes
-                            </BreezeDropdownLink>
-                            <BreezeDropdownLink :href="route('following')"
-                                :class="{ 'active': $page.url === '/following' }">
-                                Following
-                            </BreezeDropdownLink>
-                            <BreezeDropdownLink :href="route('settings/uploads')"
-                                :class="{ 'active': $page.url === '/settings/uploads' }">
-                                Uploads
-                            </BreezeDropdownLink>
-                            <BreezeDropdownLink :href="route('settings/account')"
-                                :class="{ 'active': $page.url === '/settings/account' }">
-                                Settings
-                            </BreezeDropdownLink>
-                            <BreezeDropdownLink :href="route('logout')" method="post" as="button">
-                                Log Out
-                            </BreezeDropdownLink>
-
-                        </template>
-
-                    </BreezeDropdown>
-                </div>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="showingNavigationDropdown = ! showingNavigationDropdown"
-                    class="inline-flex items-center justify-center p-2 text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path
-                            :class="{'hidden': showingNavigationDropdown, 'inline-flex': ! showingNavigationDropdown }"
-                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4 6h16M4 12h16M4 18h16" />
-                        <path
-                            :class="{'hidden': ! showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }"
-                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-        
 
         <div :class="{'block': showingNavigationDropdown, 'hidden': ! showingNavigationDropdown}" class="sm:hidden">
             <div class="pt-2 pb-3 space-y-1">
@@ -230,20 +135,7 @@
     </nav>
 
 
-    <header class="bg-white shadow " v-if="$slots.header">
-        <div class="rounded-full max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <slot name="header" />
-        </div>
-    </header>
-
-    <!-- <div class=" m-8">
-        <span id="welcome" class="font-monument flex items-center justify-center"></span>
-
-        <span class="flex items-center justify-center">
-            You have 14788 plays last 7 days
-        </span>
-    </div> -->
-
+  
 
 </template>
 <script setup>
