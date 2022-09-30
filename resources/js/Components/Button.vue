c69ff117c88e70dc1dc33c190f54faaebe48404c
soneoso <eli.dekeersgieter@gmail.com>
2022-09-30 13:55:35 +0200

    Style advertisement cards + user cards

--- resources/js/Components/Button.vue
+++ resources/js/Components/Button.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 
 <template>
-    <button :type="type" class="inline-flex border-spacing-1 border-1 border-color border-gray-400 shadow-sm items-center bg-green-300 hover:bg-black hover:text-white text-black py-2 px-8 rounded-full font-light tracking-widest  transition ease-in-out delay-50 hover:-translate-y-0.5 hover:scale-60 duration-50 antialiased">
+    <button :type="type" class="inline-flex border-spacing-1 border-1 border-color border-gray-400 shadow-sm items-center bg-green-300 hover:bg-black hover:text-white text-black py-2 px-5 rounded-full font-light tracking-widest  transition ease-in-out delay-50 hover:-translate-y-0.5 hover:scale-60 duration-50 antialiased">
         <slot />
     </button>
 </template>
