8afe85287bc65263da8274ef480407ec77c6cc0a
gychem <gychem.baert@hotmail.com>
2022-10-01 22:37:20 +0200

    update profile image to foreignId in schema

--- database/migrations/2022_09_23_140230_profile.php
+++ database/migrations/2022_09_23_140230_profile.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->string('profile_image');
+            $table->foreignId('profile_image');
             $table->text('bio');
             $table->timestamps();
         });
