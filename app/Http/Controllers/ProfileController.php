bb5e00aed256eecfaf796cc52f1b86eaf42b1cce
Levy Crols <103933738+levyCro@users.noreply.github.com>
2022-10-02 22:21:15 +0200

    adding images to profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\FreelanceAdvertisement;
 use App\Models\Profile;
+use App\Models\Upload;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,7 +17,7 @@ class ProfileController extends Controller
   public function index (User $user)
     {
 
-        $collaborations = $user->advertisements()->get();
+         $collaborations = $user->advertisements()->get();
 
         $collaborations = $collaborations->sortByDesc('created_at')->take(8);
         $followers = User::orderBy('created_at', 'desc')->take(5)->get();
@@ -28,16 +29,46 @@ class ProfileController extends Controller
             $followProfileArray = ['follow' => $follow, 'profile' => $profile];
             array_push($followProfileData, $followProfileArray);
         }
+
+        $collabs = [];
+        
+        foreach ($collaborations as $collaboration) {
+
+            $uploads = explode(",", $collaboration->uploads);
+            $uploadsResult = [];
+            
+            foreach ($uploads as $upload) {
+                if (strlen($upload) > 0) {
+                    $result = Upload::where('id', '=', $upload)->first();
+                    $uploadsResult = $result;
+                }
+            }
+            
+            $profile = Profile::Where('user_id', '=',$collaboration->user_id)->first();
+            
+            $user = User::Where('id', '=',$collaboration->user_id)->first();
+            $collab = ['user' => $user, 'collab' => $collaboration, 'upload' => $uploadsResult, 'profile' => $profile]; 
+
+            array_push($collabs, $collab); 
+        }
+
+        //dd($collabs);
         
         $profile = $user->profile()->get();
+       
+        $profileImage = Upload::where('id', '=', $profile[0]->profile_image)->first();
+        
+
         $auth = Auth::user();
         
         return Inertia::render('Profile',[
             'user' => $user,
             'collaborations' => $collaborations,
+            'collabs' => $collabs,
             'profile' => $profile,
             'followProfileData' => $followProfileData,
-            'auth' => $auth
+            'auth' => $auth,
+            'profileImage' => $profileImage
         ]);
 
 
