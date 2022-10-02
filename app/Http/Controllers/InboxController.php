194f8f1322bcbe4dba78ad0915928ed8068670de
gychem <gychem.baert@hotmail.com>
2022-10-02 11:18:42 +0200

    Add logic to send conversations to rendered page

--- app/Http/Controllers/InboxController.php
+++ app/Http/Controllers/InboxController.php
@@ -14,21 +14,79 @@ class InboxController extends Controller
 {
     public function index() {
         $user = Auth::user();
+
+        $user_auth_messages1 = $user->messages()->get();
+        $user_auth_messages2 = PrivateMessage::Where('to_user', '=', $user->id)->get();
+
+        $all_messages = $user_auth_messages1->merge($user_auth_messages2);
+        $all_messages = $all_messages->sortByDesc('created_at');
+        $all_messages = $all_messages->unique('to_user');
+        $all_messages = $all_messages->toArray();
+
+        foreach ($all_messages as $key => $message) {
+            $to_user = $message['to_user'];
+            foreach ($all_messages as $key2 => $message2) {
+                if($message2['user_id'] == $message['to_user'] && $message2['to_user'] == $message['user_id']) {
+                    if($message2['created_at'] < $message['created_at']) {
+                        unset($all_messages[$key2]);
+                    } else {
+                        unset($all_messages[$key]);
+                    }
+                }                
+            }
+        }
+
+        foreach ($all_messages as $key => $message) {
+            if($message['to_user'] == $user->id) {
+                $message['to_user'] = $message['user_id'];
+            } 
+            $to_user = User::Where('id', '=', $message['to_user'])->get();
+            $all_messages[$key]['to_user'] = $to_user[0];
+        }
+
         return Inertia::render('Inbox', [
             'user' => $user,
             'user2' => 0,
+            'all_messages' => $all_messages,
         ]); 
     }
 
-    public function date_compare($element1, $element2) {
-        $datetime1 = strtotime($element1['datetime']);
-        $datetime2 = strtotime($element2['datetime']);
-        return $datetime1 - $datetime2;
-    } 
 
     public function load_conversation(User $user) {
 
         $user_auth = Auth::user();
+
+        $user_auth_messages1 = $user_auth->messages()->get();
+        $user_auth_messages2 = PrivateMessage::Where('to_user', '=', $user_auth->id)->get();
+
+        $all_messages = $user_auth_messages1->merge($user_auth_messages2);
+        $all_messages = $all_messages->sortByDesc('created_at');
+        $all_messages = $all_messages->unique('to_user');
+        $all_messages = $all_messages->toArray();
+
+
+        foreach ($all_messages as $key => $message) {
+            $to_user = $message['to_user'];
+            foreach ($all_messages as $key2 => $message2) {
+                if($message2['user_id'] == $message['to_user'] && $message2['to_user'] == $message['user_id']) {
+                    if($message2['created_at'] < $message['created_at']) {
+                        unset($all_messages[$key2]);
+                    } else {
+                        unset($all_messages[$key]);
+                    }
+                }                
+            }
+        }
+       // dd($all_messages);
+        foreach ($all_messages as $key => $message) {
+            if($message['to_user'] == $user_auth->id) {
+                $message['to_user'] = $message['user_id'];
+            } 
+            $to_user = User::Where('id', '=', $message['to_user'])->get();
+            $all_messages[$key]['to_user'] = $to_user[0];
+        }
+
+        
 
         $user2_profile = Profile::Where('id', '=', $user->id)->get();;
         $user2_profile_img = Upload::Where('id', '=', $user2_profile[0]->profile_image)->get();
@@ -44,6 +102,7 @@ class InboxController extends Controller
             'user2' => $user,
             'user2_profile_img' => $user2_profile_img[0]->name,
             'messages' => $messages,
+            'all_messages' => $all_messages,
         ]); 
     }
 
@@ -59,6 +118,7 @@ class InboxController extends Controller
             'user_id' => $user->id,
             'to_user' => $to_user['id'],
             'message' => $message,
+            'all_messages' => $messages,
         ]);
 
         return $request;
