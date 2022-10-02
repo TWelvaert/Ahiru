d975b908eb06e5c0ec705db8cf41b4272076d214
soneoso <eli.dekeersgieter@gmail.com>
2022-10-02 19:58:35 +0200

    add old version of landing back

--- resources/js/Pages/Landing.vue
+++ resources/js/Pages/Landing.vue
@@ -1,91 +1,66 @@
 <script setup>
     import Layout from '@/Layouts/Layout.vue';
     </script>
-    
+
     <!-- Template -->
-    
+
     <template>
         <Layout>
-    
-    
-    
-            
-    
+
+
+
+            <canvas class="absolute z-10 right-0" id="canvas3d" width="1000" height="1000"></canvas>
+
             <!-- First page -->
-    
-            <div class="relative bg-black min-h-screen  ">
-            
+
+            <div class="relative bg-black min-h-screen">
+
                 <!-- Header text -->
-                
-                <div class="flex py-32 pl-72">
-                    <div class="flex">
-                        <div class="flex flex-col ">
-                            <div class="py-5">
-                                <p class="text-white text-6xl font-monument antialiased z-30 ">
-                                <p class="text-green-300">POWERFUL</p>
-                                <p>FOR</p>
-                                <p>CREATIVES</p>
-                                </p>
-                            </div>
-                            <div class="py-5">
-                                <p class="text-white text-6xl font-monument antialiased z-30">
-                                <p>ACCESSIBLE</p>
-                                <p>FOR</p>
-                                <p>EVERYONE</p>
-                                </p>
-                            </div>
-                    
-                    <!-- Buttons -->
-    
-                            <div class=" flex flex-wrap w-full gap-x-5 gap-y-3 py-5">
-                                <a href=""
-                                class="bg-green-300 z-30 hover:bg-white text-black py-3 px-4 rounded-full font-light tracking-widest  transition ease-in-out delay-50 hover:-translate-y-0.5 hover:scale-60 duration-50 antialiased ">
-                                START BUILDING
-                                </a>
-                                <a href="#community-page" v-smooth-scroll
-                                class="hover:bg-white text-white z-30 hover:text-black border-2 border-solid border-white py-3 px-8 rounded-full font-light tracking-widest  transition ease-in-out delay-50 hover:-translate-y-0.5 hover:scale-60 duration-50 antialiased">
-                                WHY AHIRU?
-                                </a>
-                            </div>
-                            <div>
-                                <p class="flex flex-wrap w-96 text-white font-roboto  text-2xl font-bold mt-5">Ahiru is a start up focussing on simplification, providing centralized tools for music creators. </p>
-                            </div>
-    
-                            </div>
-                </div>
-            
-           
-                </div>
-                <canvas class="w-24 h-auto object-contain " id="canvas3d"></canvas>
-            
-            </div>
-    
-    
-    
-    
-            <!-- FREELANCE PAGE ================================================= -->
-    
-            <div id="community-page" class="bg-black min-h-screen">
-    
-                <!-- Header text -->
-    
-                <div class="flex flex-wrap flex-col flex-shrink py-32 pl-72 ">
-                    <div class="py-5">
-                                <p class="text-white text-6xl font-monument antialiased z-30">
-                                <p class="text-green-300">FREELANCE</p>
-                                <p>AS AN</p>
-                                <p>ARTIST.</p>
-                                </p>
-                            </div>
-                            <div>
-                    <p class="flex flex-wrap w-96 py-5 text-white font-roboto  text-2xl font-bold mt-5">Book the artists via Ahiru & get hired to work as freelance on projects or find other artists to collaborate.
+
+                <div class="grid grid-rows-2 gap-10 grid-cols-2 p-24 pl-72 ">
+                    <p class="text-white text-6xl font-monument antialiased z-30">
+                    <p class="text-green-300">POWERFUL</p> FOR <br>
+                    CREATIVES.
                     </p>
+
+                    <p class="text-white text-6xl font-monument antialiased col-span-2 z-30">ACCESSIBLE <br> FOR <br>
+                        EVERYONE.
+                    </p>
+
+                    <!-- Buttons -->
+
+                    <div class="mt-4 space-x-4 z-40 ">
+                        <a href=""
+                            class="bg-green-300 z-30 hover:bg-white text-black py-3 px-5 rounded-full font-light tracking-widest  transition ease-in-out delay-50 hover:-translate-y-0.5 hover:scale-60 duration-50 antialiased">
+                            START BUILDING
+                        </a>
+                        <a href="#community-page" v-smooth-scroll
+                            class="hover:bg-white text-white z-30 hover:text-black border-2 border-solid border-white py-3 px-5 rounded-full font-light tracking-widest  transition ease-in-out delay-50 hover:-translate-y-0.5 hover:scale-60 duration-50 antialiased">
+                            WHY AHIRU?
+                        </a>
+                    </div>
+                    <p class="text-white font-roboto col-span-2 z-40 text-2xl font-bold mt-5">Ahiru is a start up focussing
+                        on <br> simplification, providing centralized <br> tools for music creators. </p>
                 </div>
-            
-    
-                    
-    
-    
+            </div>
+
+
+
+
+            <!-- FREELANCE PAGE ================================================= -->
+
+            <div id="community-page" class="relative  bg-black min-h-screen">
+
+                <!-- Header text -->
+
+                <div class="grid grid-rows-2 gap-1 grid-cols-2 p-24 pl-72 ">
+                    <p class="text-white text-6xl font-monument antialiased z-30">
+                    <p class="text-green-300">FREELANCE</p> AS AN <br>
+                    ARTIST.
+                    <p class="text-white font-roboto z-40 text-2xl font-bold mt-5">Book the artists via Ahiru &
+                        <br> get hired to work as freelance on projects <br> or find other artists to collaborate.
+                    </p>
+                    </p>
                     <a href="#"
                         class="p-6 mr-48 max-w-sm col-end-5 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                         <img src="../../assets/images/070c4ae0e59af72c222e2756c87baa1a.gif" alt="">
@@ -97,11 +72,11 @@
                     </a>
                 </div>
             </div>
-    
-    
+
+
             <!-- UPLOAD ================================================ -->
-    
-    
+
+
             <div class="bg-black min-h-screen text-center pt-8"
                 id="upload-page">
                 <div class="">
@@ -113,14 +88,14 @@
                             Calling all creators
                         </span>
                     </div>
-    
+
                     <p class="text-white text-xl font-light mt-11">
                         Get on AHIRU to connect with fans, share your sounds, and grow your audience. <br> What are you
                         waiting
                         for?
                     </p>
-    
-    
+
+
                 </div>
                 <div class="ml-92 mt-16">
                     <a href=""
@@ -129,13 +104,13 @@
                     </a>
                 </div>
             </div>
-    
-    
-    
+
+
+
             <!-- CONTACT ================================================= -->
-    
-    
-    
+
+
+
             <div class=" bg-white min-h-screen">
                 <div class='bg-purple-400 bg-gradient-to-b from-cyan-700 to-blue-300 height-'>
                     <p></p>
@@ -146,7 +121,7 @@
                         for
                         free.</h2>
                 </article>
-    
+
                 <section class="grid place-items-center pt-24">
                     <article
                         class="text-center p-10 min-h-116 max-w-3xl w-full bg-black rounded-xl text-gray-100 xl:col-span-2 transform duration-500 hover:-translate-y-1">
@@ -156,9 +131,9 @@
                             giant leap for your career
                         </h1>
                         <div class="mt-20">
-    
+
                         </div>
-    
+
                         <div class="flex items-center justify-center space-x-4">
                             <a href=""
                                 class="bg-green-300 hover:bg-white text-black py-3 px-5 rounded-full font-light tracking-widest font transition ease-in-out delay-50 hover:-translate-y-0.5 hover:scale-60 duration-50 antialiased">
@@ -166,34 +141,34 @@
                             </a>
                         </div>
                         <div class="mt-6 flex items-center justify-center space-x-4 ">
-    
+
                             <span class="text-center text-xs antialiased">already have an acount?</span>
                             <a href="#community-page" v-smooth-scroll
                                 class="hover:bg-white text-white hover:text-black border-2 border-solid border-white py-3 px-5 rounded-full tont-light tracking-widest transition ease-in-out delay-50 hover:-translate-y-0.5 hover:scale-60 duration-50 antialiased">
                                 SIGN IN
                             </a>
-    
+
                         </div>
                     </article>
                 </section>
             </div>
-    
-    
+
+
         </Layout>
     </template>
-    
+
     <script>
     import { Application } from '@splinetool/runtime';
-    
-    
+
+
     export default {
-    
-    
+
+
         mounted() {
-    
+
             // make sure you have a canvas in the body
             const canvas = document.querySelector('#canvas3d');
-    
+
             console.log(canvas)
             // start the application and load the scene
             const spline = new Application(canvas);
@@ -201,12 +176,8 @@
             spline.load('https://prod.spline.design/pHiNKU0XXk4eXYUX/scene.splinecode');
         }
     }
-    
-    
-    
-    </script>
-    
-    
-    
 
+
+
+    </script>
 
